d513a2dd253ca2bccd523d8f0202a608306d4fbe
Arjun Varshney <varshneyarjun49@gmail.com>
2022-09-26 18:54:04 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import UserAuth from "./Pages/UserAuth";
+
+function App() {
+  return (
+    <div>
+      <UserAuth/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Login.jsx
@@ -0,0 +1,103 @@
+import * as React from "react";
+import {
+  Avatar,
+  Button,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+} from "@mui/material";
+import Copyright from "./Copyright";
+import { styled } from "@mui/material/styles";
+
+const Login = (props) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  const CustomContainer = styled(Container)`
+    border: 1px solid rgb(0 0 0/0.1);
+    border-radius: 10px;
+    margin-top: 50px;
+  `;
+
+  return (
+    <CustomContainer component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 5,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }} />
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link
+                href="#"
+                variant="body2"
+                onClick={() => props.setAuth("signup")}
+              >
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </CustomContainer>
+  );
+};
+
+export default Login;
